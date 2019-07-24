289c274534a885a4f29990679d1f2a70f7b66571
wangchunzi <pankeng1988w@aliyun.com>
2019-07-24 15:48:28 +0800

    1

--- src/main/java/weixinkeji/vip/jweb/validate/_common/_ValidateDataCenter.java
+++ src/main/java/weixinkeji/vip/jweb/validate/_common/_ValidateDataCenter.java
@@ -1,12 +1,9 @@
 package weixinkeji.vip.jweb.validate._common;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Parameter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import weixinkeji.vip.jweb.scan.JWPScanClassFactory;
 import weixinkeji.vip.jweb.validate.init._0_init_validateLib;
 
 public class _ValidateDataCenter {
@@ -14,11 +11,6 @@ public class _ValidateDataCenter {
 	 * 正则表达式库
 	 */
 	static final Map<String, JWebMVCValidateVo> sysValidationLib = new HashMap<>();
-	// 用户vo最终表达式
-	private static final Map<Class<?>, Map<Field, JWebMVCValidateVo>> voValidate = new HashMap<>();
-
-	// 方法参数上的vo最终表达式
-	private static final Map<Class<?>, Map<Parameter, JWebMVCValidateVo>> webParamValidate = new HashMap<>();
 	private static boolean isInit = false;
 
 	/**
@@ -42,10 +34,8 @@ public class _ValidateDataCenter {
 		}
 		isInit = true;
 		// 初始化：
-		// JWebMvc.properties
-		List<Class<?>> scanclass = JWPScanClassFactory.getClassByFilePath("xxxxxxxxxx");
 		// 初始化正则表达式库
-		new _0_init_validateLib(scanclass).initConfig(sysValidationLib);
+		new _0_init_validateLib(list).initConfig(sysValidationLib);
 
 	}
 

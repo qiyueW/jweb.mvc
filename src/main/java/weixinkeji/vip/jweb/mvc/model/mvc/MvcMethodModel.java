9009c430bd313d038e2516aabe5a4c85383fddc5
wangchunzi <pankeng1988w@aliyun.com>
2019-07-30 16:34:59 +0800

    1

--- src/main/java/weixinkeji/vip/jweb/mvc/model/mvc/MvcMethodModel.java
+++ src/main/java/weixinkeji/vip/jweb/mvc/model/mvc/MvcMethodModel.java
@@ -1,8 +1,45 @@
 package weixinkeji.vip.jweb.mvc.model.mvc;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MvcMethodModel {
+	// 存放 url映射Method 的集合
+	private static final Map<String, MvcMethodModel> urlMapMethod = new HashMap<>();
+	// urlMapMethod的开关-
+	private static boolean urlMapMethod_open = true;
+
+	/**
+	 * 取得mvc方法模型
+	 * 
+	 * @param url 请求方法
+	 * @return MvcMethodModel mvc方法模型
+	 */
+	public static MvcMethodModel getMvcMethodModel(String url) {
+		return urlMapMethod.get(url);
+	}
+
+	/**
+	 * 放入mvc方法模型
+	 * 
+	 * @param url   请求方法
+	 * @param model MvcMethodModel 方法模型
+	 */
+	public static void regMvcMethodModel(String url, MvcMethodModel model) {
+		if (urlMapMethod_open) {
+			urlMapMethod.put(url, model);
+		}
+	}
+
+	/**
+	 * 关闭配置，关闭后，无法再加入方法模型
+	 */
+	public static void closeConfig() {
+		urlMapMethod_open = false;
+	}
+
+	// ---------------------------------------------------------------------------
 
 	/**
 	 * 绑定的请求路径
@@ -12,15 +49,31 @@ public class MvcMethodModel {
 	 * 方法
 	 */
 	public Method method;
-	
-	// 方法包含的参数模型
-	private MvcMethodParameterModel[] paramMode = new MvcMethodParameterModel[method.getParameterCount()];
-	
-	
-	
-	
-	public Object doMethod(Object mvcObj) throws Exception {
-		return method.invoke(mvcObj, null);
+
+	/**
+	 * 方法包含的参数模型
+	 */
+	public final MvcMethodParameterModel[] paramMode;
+	/**
+	 * 方法返回类型
+	 */
+	public final MvcMethodReturnModel returnModel;
+
+	public MvcMethodModel(Method method) {
+		this.method = method;
+		this.paramMode = new MvcMethodParameterModel[method.getParameterCount()];
+		this.returnModel = new MvcMethodReturnModel(method);
 	}
-	
+
+	/**
+	 * 执行方法
+	 * 
+	 * @param mvcObj 对象
+	 * @return Object 返回的应该是框架的路径或直接为null
+	 * @throws Exception 可能发生的错误
+	 */
+	public Object doMethod(Object mvcObj) throws Exception {
+		return method.invoke(mvcObj);
+	}
+
 }
--- src/main/java/weixinkeji/vip/jweb/mvc/model/mvc/MvcMethodReturnModel.java
+++ src/main/java/weixinkeji/vip/jweb/mvc/model/mvc/MvcMethodReturnModel.java
@@ -15,25 +15,47 @@ public class MvcMethodReturnModel {
 	/**
 	 * 方法返回 类型
 	 */
-	public Class<?> returnType;
+	public final Class<?> returnType;
 
 	/**
 	 * -1：无返回（当 returnType=void.class时）<br>
 	 * 0：控制类 通过判断方法上的注解 @JWebReturnType("C")小写或大写都行<br>
 	 * 1：jsp视图(默认）通过判断方法上的注解 @JWebReturnType("jsp")小写或大写都行,没有注解，默认jsp<br>
 	 */
-	public int returnUrlType;
+	public final int returnUrlType;
+	// 是否有返回类型。通过 this.isNotReturn = this.returnType == void.class;
+	// 取得
+	private final boolean isNotReturn;
+
+	public MvcMethodReturnModel(Method m) {
+		this.returnType = m.getReturnType();
+		this.returnUrlType = getMethodReturnType(m);
+		this.isNotReturn = this.returnType == void.class;
+	}
 
 	// 框架定义，只返回的是String类型或void类型
 	// return "xxx";表示跳转 return ":xxx";表示重定向
-	public void say() {
+	/**
+	 * 得取符合http要求的返回路径。当为null时，表示没有返回值，或返回了错误的空值
+	 * 
+	 * @param url 框架封装的路径
+	 * @return String
+	 */
+	public String getUrl(String url) {
+		if (this.isNotReturn || url.length() == 0) {
+			return null;
+		}
+		char[] urlc = url.toCharArray();
+		if (urlc[0] != ' ') {// 如果非空开头，直接判断是否以:开头，是的话，进行切除
+			return urlc[0] == ':' ? String.copyValueOf(urlc, 1, urlc.length - 1) : url;
+		}
+		int i = 0;
+		while (urlc[i] == ' ') {
+			i++;
+		}
+		return urlc[i] == ':' ? String.copyValueOf(urlc, i + 1, urlc.length - i - 1) : url;
+	}
 
-	}
-	public MvcMethodReturnModel(Method m) {
-		this.returnType=m.getReturnType();
-		this.returnUrlType=getMethodReturnType(m);
-	}
-	
 	/**
 	 * -1：无返回（当 returnType=void.class时）<br>
 	 * 0：控制类 通过判断方法上的注解 @JWebReturnType("C")小写或大写都行<br>
@@ -41,23 +63,14 @@ public class MvcMethodReturnModel {
 	 */
 	private static int getMethodReturnType(Method m) {
 		JWebReturnType jt = m.getAnnotation(JWebReturnType.class);
-		if (null == jt
-				||(null!=jt.value()&&jt.value().trim().equalsIgnoreCase("jsp"))||(jt.type().trim().equalsIgnoreCase("jsp"))
-			) {
+		if (null == jt || (null != jt.value() && jt.value().trim().equalsIgnoreCase("jsp"))
+				|| (jt.type().trim().equalsIgnoreCase("jsp"))) {
 			return 1;// jsp视图
-		}else if((null!=jt.value()&&jt.value().trim().equalsIgnoreCase("c"))||(jt.type().trim().equalsIgnoreCase("c"))) {
+		} else if ((null != jt.value() && jt.value().trim().equalsIgnoreCase("c"))
+				|| (jt.type().trim().equalsIgnoreCase("c"))) {
 			return 0;// 控制区的路径
 		}
-		return 1;//其他后续视图的支持与整合（暂时不写上）
+		return 1;// 其他后续视图的支持与整合（暂时不写上）
 	}
-	
-//	
-//	
-//	
-//	public static void main(String args[]) {
-//		for (Method m : MvcMethodReturnModel.class.getDeclaredMethods()) {
-//			System.out.println(m.getReturnType());
-//		}
-//	}
 
 }

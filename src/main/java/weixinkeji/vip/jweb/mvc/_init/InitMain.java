ee02329dcf7298e008555bf06e1d2576d4e0d7f7
wangchunzi <pankeng1988w@aliyun.com>
2019-08-06 15:12:20 +0800

    1

---
+++ src/main/java/weixinkeji/vip/jweb/mvc/_init/InitMain.java
@@ -0,0 +1,11 @@
+package weixinkeji.vip.jweb.mvc._init;
+
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+
+public class InitMain {
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+}

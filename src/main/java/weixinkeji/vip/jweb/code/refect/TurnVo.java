a2e516588e3077205c441312a6909f01797d377d
wangchunzi <pankeng1988w@aliyun.com>
2019-08-14 17:51:26 +0800

    1

--- src/main/java/weixinkeji/vip/jweb/code/refect/TurnVo.java
+++ src/main/java/weixinkeji/vip/jweb/code/refect/TurnVo.java
@@ -1,11 +1,19 @@
 package weixinkeji.vip.jweb.code.refect;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import weixinkeji.vip.jweb.code.refect.string_type._SysStringVoModel;
+
 /**
  * String 类型转其他类型处理中心
  * 
  * @author wangchunzi
  *
  */
-public class StringToYourType {
+public class TurnVo {
 
+	private static Map<Class<?>,_SysStringVoModel> map=new HashMap<>();
+	
+	
 }
--- src/main/java/weixinkeji/vip/jweb/code/refect/string_type/_SysStringVoModel.java
+++ src/main/java/weixinkeji/vip/jweb/code/refect/string_type/_SysStringVoModel.java
@@ -1,5 +1,26 @@
 package weixinkeji.vip.jweb.code.refect.string_type;
 
-public interface StringVoModel {
+public abstract class _SysStringVoModel {
+	/**
+	 * 转换成的类型
+	 */
+	public final Class<?> targetClass;
 
+	/**
+	 * 值转换
+	 * 
+	 * @param vo
+	 * @return
+	 */
+	abstract Object toYourType(String vo);
+
+	/**
+	 * 构造函数
+	 * 
+	 * @param sourcesClaass Class
+	 * @param targetClass   Class
+	 */
+	public _SysStringVoModel(Class<?> sourcesClaass, Class<?> targetClass) {
+		this.targetClass = targetClass;
+	}
 }
